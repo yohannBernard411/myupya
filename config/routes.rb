042bc086de621f8df48bb9cd60e2e4dd63a7f6af
Lilian Besse <lilianbesse31@gmail.com>
2020-06-23 16:55:39 +0200

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  get 'steps/index'
   devise_for :users
   root to: 'pages#home'
+  resources :questions, only: :show
+  resources :answers, only: :create
+  resources :steps, only: :index
+  resources :coachs, only: :show
+  resources :videos, only: :show
+  resources :articles, only: :show
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
